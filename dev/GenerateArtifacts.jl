cbb75890b716cbd7ab61e7366d50d0a9f3cb9fe5
mpersico <mpersico@ubuntu-zstd.fedora>
2022-11-24 11:56:22 -0500

    Add DataFrames dep, Artifacts.toml

---
+++ dev/GenerateArtifacts.jl
@@ -0,0 +1,9 @@
+#=
+Script to generate Julia artifacts, which represent
+fixed, immutable containers for data, in this instance
+any dataset utilized for creating Zstd dictionaries. All 
+artifacts are located in the artifacts/ directory, and 
+their entries are listed in the Artifacts.toml file.
+Docs: https://pkgdocs.julialang.org/v1/artifacts/
+=#
+
--- src/Persico2022.jl
+++ src/Persico2022.jl
@@ -1,8 +1,8 @@
-module FinalProjectBIOL480
+module Persico2022
 
 using PyCall
 
 zstd = pyimport_conda("pyzstd", "pyzstd=0.15.3")
-
+println(zstd.zstd_version_info)
 
 end # module FinalProjectBIOL480
